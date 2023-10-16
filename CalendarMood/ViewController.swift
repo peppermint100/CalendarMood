0895e4092adfc87f2d437a4312155e2daa076427
peppermint100 <krpeppermint100@gmail.com>
2023-10-16 20:17:21 +0900

    init

---
+++ CalendarMood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalendarMood
+//
+//  Created by peppermint100 on 10/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
