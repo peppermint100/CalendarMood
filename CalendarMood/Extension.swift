f9061d1237b99bd84367ad431ed04622ac472618
peppermint100 <krpeppermint100@gmail.com>
2023-10-16 20:59:26 +0900

    Localization 적용

---
+++ CalendarMood/Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.swift
+//  CalendarMood
+//
+//  Created by peppermint100 on 10/16/23.
+//
+
+import Foundation
+
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
--- CalendarMood/ViewController.swift
+++ CalendarMood/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "calendar".localized
+        label.textColor = .label
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.addSubview(label)
+        view.backgroundColor = .green
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        label.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+    }
 }
 
